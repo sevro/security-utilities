1f764bca1f5da43aac356fccfac7b2f4d978f146
Derek <dgodd001@odu.edu>
2016-12-20 18:15:57 -0500

    run each ping in background, decrease wait time
    Huge performance increase

--- bash/pingsweep/pingsweep.sh
+++ bash/pingsweep/pingsweep.sh
@@ -5,6 +5,6 @@ then
     echo "Example: ./pingsweep_type_c.sh 192.168.1"
 else
 for x in `seq 1 254`; do
-    ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f4 | sed 's/.$//'
+    ping -c 1 -i 0.2 -W 1 $1.$x | grep "64 bytes" | cut -d " " -f4 | sed 's/.$//' &
 done
 fi
