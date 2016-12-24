a6b5eb723d581c2f5d6b06f0b2434c0f747d2c3b
Derek <dgodd001@odu.edu>
2016-12-24 15:57:42 -0500

    reverse DNS lookup init

---
+++ bash/reverse_dns/README.md
@@ -0,0 +1,9 @@
+# Reverse DNS Lookup
+
+*   Input: An IP address, a range, and a filter
+*   Output: A list of hostnames and ip addresses
+
+# Example Usage
+
+    bash reverse_dns.sh [ip address] [range start] [range stop] [filter]
+    bash reverse_dns.sh 38.100.193 72 91 megacorp
---
+++ bash/reverse_dns/reverse_dns.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Reverse DNS Lookup
+# Input: An IP address, a range, and a filter
+# Output: 
+#
+# Example Usage: bash reverse_dns.sh [ip address] [range start] [range stop] [filter]
+#                bash reverse_dns.sh 38.100.193 72 91 megacorp
+
+echo "[*] Scanning $1 in range $2 to $3"
+for ip in $(seq "$2" "$3"); do
+    host $1.$ip | grep "$4" | cut -d " " -f 1,5 >> $1.subdomains.txt
+done
+echo "[*] filtered all domains without the string $4"
+
+sort -u $2.subdomains.txt > tmp && mv tmp $2.subdomains.txt
+
+NUM_SUBDOMAINS=`wc -l $2.subdomains.txt | cut -d " " -f 1`
+echo "[*] Found $NUM_SUBDOMAINS subdomains for $1"
+
+exit 0
