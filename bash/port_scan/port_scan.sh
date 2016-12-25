c8b8446060e1f74f938f226d99157578cc389216
Derek <dgodd001@odu.edu>
2016-12-25 14:17:45 -0500

    add status messages

--- bash/port_scan/port_scan.sh
+++ bash/port_scan/port_scan.sh
@@ -6,10 +6,12 @@ then
 else
     mkdir -p target
     for ip in $(cat "$1"); do
-        nmap -nsSU $ip -p 1-65355 > target/portscan.$ip.txt &
+        echo "[*] Scanning ip address $ip"
+        nmap -nsSU -A -sV --version-all $ip -p 1-65355 > target/portscan.$ip.txt &
         pid="$!"
         wait $pid || let "RESULT=1"
         if [ "$RESULT" == "1" ]; then
+            echo "[*] It blew up"
             exit 1
         fi
     done
